46ed4d7a151e181d61104e766716922e60525899
superemma <43006502+superemma@users.noreply.github.com>
2018-09-26 17:06:45 -0400

    Update d3v.js

--- d3v.js
+++ d3v.js
@@ -230,21 +230,6 @@ d3.select("#show-voronoi")
 // dot attributes
 var dotsAttr = {cx: function(d){return d[0]},
                 cy:function(d){return d[1]},
-                r: 5,
+                r: 3,
                 fill: "blue"}
 
-// set up drag for circles
-var drag = d3.behavior.drag()
-    .on("drag", dragmove);
-
-function dragmove(d) {
-    d3.select(this)
-      .attr("cx", d3.event.x)
-      .attr("cy", d3.event.y);
-
-    this.__data__ = [d3.event.x, d3.event.y]
-
-    updateDots();
-
-    endOfLastDrag = Date.now();
-}
